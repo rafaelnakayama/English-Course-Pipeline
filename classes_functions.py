6be70a60d213d60326f53eb25c52b8aa819b71c4
Rafael Nakayama <RafaelNakayama2003@gmail.com>
2025-11-11 15:52:37 -0300

    Beta 1.6.9 - 'adicionar_material' created

--- classes_functions.py
+++ classes_functions.py
@@ -24,40 +24,67 @@ def adicionar_material(id_param, tipo_param):
     caminho_exercicios_aluno_csv = os.path.join(os.path.dirname(__file__), "data", "historicos", f"{id_param}_exercicios.csv")
 
     # Inicia as variaveis que vao receber diferentes valores dependendo dos inputs
+    caminho_destino = None
     caminho_relativo = None
     nome_material = None
 
     if tipo_param == 1:
         caminho_relativo = ARQUIVOS['aulas']
+        caminho_destino = os.path.join(os.path.dirname(__file__), "data", "historicos", f"{id_param}_aulas.csv")
 
         nome_material = str(input("\033[32mInforme o nome da Aula: \033[1;31m")).strip()
-        material_existe(nome_material, tipo_param)
+        material_existe(nome_material, tipo_param) # Vai retornar True ou False
         while (material_existe(nome_material, tipo_param) == False):
             print("\033[1;31mEsta aula não está no banco de dados.\033[1;31m")
             nome_material = str(input("\033[32mInforme o nome da Aula: \033[1;31m"))
 
     elif tipo_param == 2:
         caminho_relativo = ARQUIVOS['textos']
+        caminho_destino = os.path.join(os.path.dirname(__file__), "data", "historicos", f"{id_param}_textos.csv")
 
         nome_material = str(input("\033[32mInforme o nome do Texto: \033[1;31m")).strip()
-        material_existe(nome_material, tipo_param)
+        material_existe(nome_material, tipo_param) # Vai retornar True ou False
         while (material_existe(nome_material, tipo_param) == False):
             print("\033[1;31mEste Texto não está no banco de dados.\033[1;31m")
             nome_material = str(input("\033[32mInforme o nome do Texto: \033[1;31m"))
 
     else:
         caminho_relativo = ARQUIVOS['exercicios']
+        caminho_destino = os.path.join(os.path.dirname(__file__), "data", "historicos", f"{id_param}_exercicios.csv")
 
         nome_material = str(input("\033[32mInforme o nome Exercicio: \033[1;31m")).strip()
-        material_existe(nome_material, tipo_param)
+        material_existe(nome_material, tipo_param) # Vai retornar True ou False
         while (material_existe(nome_material, tipo_param) == False):
             print("\033[1;31mEste Exercicio não está no banco de dados.\033[1;31m")
             nome_material = str(input("\033[32mInforme o nome do Exercicio: \033[1;31m"))
 
-    with open(caminho_relativo, newline='') as arquivocsv:
-        df = pd.read_csv(arquivocsv)
-        
+    id_material = pegar_id_por_nome_M(nome_material, caminho_relativo)
 
+    # Adicionando ao respectivo csv
+    df_origem = pd.read_csv(caminho_relativo)
+
+    df_indexado = df_origem.set_index('id')
+
+    linha_copia = df_indexado.loc[id_material].copy()
+
+    df_destino = pd.read_csv(caminho_destino)
+
+    df_destino.loc[len(df_destino)] = linha_copia
+
+def pegar_id_por_nome_M(nome, caminho):
+
+    df = pd.read_csv(caminho)
+
+    df['name'] = df['name'].str.strip()
+
+    # Filtra o aluno correspondente
+    material_encontrado = df[df['name'] == nome]
+
+    if not material_encontrado.empty:
+        id_material = material_encontrado['id'].iloc[0]
+        return id_material
+    else:
+        return None
 
 
 
